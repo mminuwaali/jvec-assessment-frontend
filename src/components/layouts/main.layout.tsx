d4d86c9b5111d24f38b5b274449732eddb0c0473
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-18 18:12:23 +0100

    setup some logics in the main layout to close authentication form after logging in

--- src/components/layouts/main.layout.tsx
+++ src/components/layouts/main.layout.tsx
@@ -1,8 +1,11 @@
 import $header from "../header"
 import { $form } from "../forms"
 import { createPortal } from "react-dom"
-import { JSXElementConstructor, useMemo } from "react"
+import { JSXElementConstructor, useEffect, useMemo } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
+import { useAppDispatch, useAppSelector } from "../../store"
+import { loginForm, registerForm } from "../../constants/forms"
+import { login, register } from "../../store/slice/account.slice"
 
 type ComponentObjectType = {
     [key: string]: {
@@ -13,22 +16,45 @@ type ComponentObjectType = {
 
 export default ($component: JSXElementConstructor<any>) => () => {
     const nav = useNavigate()
+    const dispatch = useAppDispatch()
     const { hash, pathname } = useLocation()
+    const { user, error, loading } = useAppSelector(state => state.account)
+
+    useEffect(() => {
+        if (user && /(login|register)$/.test(hash)) nav(pathname, { replace: true })
+    }, [hash, user])
+
+    const components: ComponentObjectType = ({
+        login: {
+            component: $form,
+            props: {
+                ...loginForm,
+                onSubmit: (data: Partial<UserType>) => dispatch(login(data))
+            },
+        },
+        register: {
+            component: $form,
+            props: {
+                ...registerForm,
+                onSubmit: (data: Partial<UserType>) => dispatch(register(data))
+            },
+        },
+    })
 
     const handleClosePortal = () => nav(pathname, { replace: true })
 
-    const components: ComponentObjectType = ({
-        login: { props: {}, component: $form },
-        register: { props: {}, component: $form },
-    })
-
-    const form = useMemo(() => components[hash.slice(1)], [hash]);
+    const form = useMemo(() => components[hash.slice(1)], [hash])
 
     return <>
         <$header />
         <main className="grow flex flex-col items-center w-full">
             <$component />
         </main>
-        {createPortal(form && <form.component {...form.props} onClose={handleClosePortal} />, document.querySelector<HTMLDivElement>('#portal')!)}
+        {createPortal(form && <form.component
+            error={error}
+            loading={loading}
+            {...form.props as any}
+            onClose={handleClosePortal}
+        />, document.querySelector<HTMLDivElement>('#portal')!)}
     </>
 }
