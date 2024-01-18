7f87f61ea1c393dc7b3ddbfe14056698a75ffc3b
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-18 16:48:35 +0100

    registered the account and contact slice to general store and made some subscriptions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,6 @@
+import * as account from './slice/account.slice'
+import * as contact from './slice/contact.slice'
+
 import storage from 'redux-persist/lib/storage'
 import { persistStore, persistReducer } from 'redux-persist'
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
@@ -10,7 +13,10 @@ const store = configureStore({
             key: 'contact-manager',
             blacklist: ['contact'],
         },
-        combineReducers({})
+        combineReducers({
+            account: account.default,
+            contact: contact.default,
+        })
     ),
 })
 
@@ -19,3 +25,10 @@ export const persistor = persistStore(store)
 
 export const useAppDispatch: () => typeof store.dispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector
+
+store.subscribe(() => {
+    const state = store.getState()
+
+    if (state.account.error) setTimeout(() => store.dispatch(account.clearError()), 5000)
+    if (state.contact.error) setTimeout(() => store.dispatch(contact.clearError()), 5000)
+})
