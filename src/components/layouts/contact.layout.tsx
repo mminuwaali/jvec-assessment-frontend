b97d495099e0fd2ba6f67354a6d9bcd61a24d638
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-19 00:24:19 +0100

    setup authentication logic and contact form components

--- src/components/layouts/contact.layout.tsx
+++ src/components/layouts/contact.layout.tsx
@@ -2,9 +2,11 @@ import $navbar from "../navbar"
 import { $form } from "../forms"
 import $sidebar from "../sidebar"
 import { createPortal } from "react-dom"
-import { JSXElementConstructor, useMemo } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import { useAppDispatch, useAppSelector } from "../../store"
+import { getContacts } from "../../store/slice/contact.slice"
+import { JSXElementConstructor, useEffect, useMemo } from "react"
+import { createContactForm, updateContactForm } from "../../constants/forms"
 
 type ComponentObjectType = {
     [key: string]: {
@@ -17,16 +19,22 @@ export default ($component: JSXElementConstructor<any>) => () => {
     const nav = useNavigate()
     const dispatch = useAppDispatch()
     const { hash, pathname } = useLocation()
-    const { error, loading } = useAppSelector(state => state.contact)
+    const user = useAppSelector(state => state.account.user)
+    const { data, error, loading } = useAppSelector(state => state.contact)
+
+    useEffect(() => {
+        if (user && data.length === 0) dispatch(getContacts())
+        else nav('/', { replace: true })
+    }, [user])
 
     const components: ComponentObjectType = ({
-        'create-contact': {
-            props: {},
+        'new-contact': {
             component: $form,
+            props: { ...createContactForm },
         },
         'update-contact': {
-            props: {},
             component: $form,
+            props: { ...updateContactForm },
         },
     })
 
@@ -44,8 +52,9 @@ export default ($component: JSXElementConstructor<any>) => () => {
         </main>
         {createPortal(form && <form.component
             error={error}
-            {...form.props}
             loading={loading}
+            onSubmit={() => { }}
+            {...form.props as any}
             onClose={handleClosePortal}
         />, document.querySelector<HTMLDivElement>('#portal')!)}
     </section>
