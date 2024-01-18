e1f50348bcfd045f1ebb4fb3c454b03cc0907d73
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-18 17:25:57 +0100

    setup the form popup logic implementation

--- src/components/layouts/main.layout.tsx
+++ src/components/layouts/main.layout.tsx
@@ -1,11 +1,34 @@
 import $header from "../header"
-import { JSXElementConstructor } from "react"
+import { $form } from "../forms"
+import { createPortal } from "react-dom"
+import { JSXElementConstructor, useMemo } from "react"
+import { useLocation, useNavigate } from "react-router-dom"
+
+type ComponentObjectType = {
+    [key: string]: {
+        component: typeof $form,
+        props: object | FormPropType,
+    }
+}
 
 export default ($component: JSXElementConstructor<any>) => () => {
+    const nav = useNavigate()
+    const { hash, pathname } = useLocation()
+
+    const handleClosePortal = () => nav(pathname, { replace: true })
+
+    const components: ComponentObjectType = ({
+        login: { props: {}, component: $form },
+        register: { props: {}, component: $form },
+    })
+
+    const form = useMemo(() => components[hash.slice(1)], [hash]);
+
     return <>
         <$header />
         <main className="grow flex flex-col items-center w-full">
             <$component />
         </main>
+        {createPortal(form && <form.component {...form.props} onClose={handleClosePortal} />, document.querySelector<HTMLDivElement>('#portal')!)}
     </>
 }
