30c3d4635bd2899796243e0d5a5307ceee6cd3e1
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-18 16:34:47 +0100

    created the banner component

---
+++ src/components/banner.tsx
@@ -0,0 +1,3 @@
+export default () => {
+    return <section className="h-4/6 lg:h-screen bg-main bg-cover relative before:absolute before:bottom-0 before:left-[40%] before:h-full before:md:h-[initial] before:md:w-full before:aspect-square before:rounded-full before:shadow-xl before:backdrop-blur"></section>
+}
