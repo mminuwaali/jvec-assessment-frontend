c775aa0441e374184cac16494d8b4bd9f6242947
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-18 17:25:36 +0100

    setup the login and register form data

---
+++ src/constants/forms.ts
@@ -0,0 +1,61 @@
+import { MdEmail, MdPassword } from "react-icons/md";
+
+export const loginForm: Omit<FormPropType, 'onClose' | 'onSubmit'> = {
+    button: "log in",
+    title: "welcome back",
+    links: [
+        {
+            to: "/signup",
+            title: "Don't have an account? *register*"
+        }
+    ],
+    inputs: [
+        {
+            type: "email",
+            name: "email",
+            icon: MdEmail,
+            required: true,
+            placeholder: "Enter your email",
+        },
+        {
+            required: true,
+            type: "password",
+            name: "password",
+            icon: MdPassword,
+            placeholder: "Enter your password",
+        },
+    ],
+};
+
+export const registerForm: Omit<FormPropType, 'onClose' | 'onSubmit'> = {
+    button: "register",
+    title: "register here",
+    links: [
+        {
+            to: "/signin",
+            title: "Already having an account? *login here*"
+        }
+    ],
+    inputs: [
+        {
+            type: "text",
+            required: true,
+            name: "username",
+            placeholder: "Enter your username",
+        },
+        {
+            type: "email",
+            name: "email",
+            icon: MdEmail,
+            required: true,
+            placeholder: "Enter your email",
+        },
+        {
+            required: true,
+            type: "password",
+            name: "password",
+            icon: MdPassword,
+            placeholder: "Enter a strong password",
+        },
+    ],
+};
