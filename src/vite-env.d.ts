ee3b72eb2ff7a0ed6ace79e6b555af9283e91003
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-18 16:40:22 +0100

    created the global state type interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+declare type StateType<T extends object, E extends object> = E & {
+    data: T[]
+    item?: null | T
+    loading: boolean
+    error: string | boolean
+}
