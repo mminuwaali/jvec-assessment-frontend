5813519c4322f9d50e179370bdbb09ada427fc90
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-18 16:41:15 +0100

    created the account and contact type interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,21 @@ declare type StateType<T extends object, E extends object> = E & {
     loading: boolean
     error: string | boolean
 }
+
+declare type ContactType = {
+    id: number
+    lastName: string
+    firstName: string
+    phoneNumber: string
+}
+
+declare type UserType = {
+    id: number
+    email: string
+    username: string
+    lastName: string
+    password?: string
+    firstName: string
+    phoneNumber: string
+    profile: null | string
+}
