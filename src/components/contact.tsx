79ad0df6dce3f6a95cccd551f91c828a8b9d8073
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-01-19 01:08:10 +0100

    set an eleement for indicating empty data in contact list rendering

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,11 +1,13 @@
 import { useMemo } from 'react'
 import $contact from './cards/contact'
 import { useAppSelector } from '../store'
+import { useLocation } from 'react-router-dom'
 import { useSearch } from '../store/context/search.context'
 import { convertArrayToObject } from '../utilities/function'
 
 export default () => {
     const { q } = useSearch()
+    const { hash } = useLocation()
     const { data, loading } = useAppSelector(state => state.contact)
 
     const filteredContacts = useMemo(() => {
@@ -23,8 +25,12 @@ export default () => {
     const contacts = useMemo(() => convertArrayToObject(filteredContacts), [q, filteredContacts])
 
     return <div className="w-full flex flex-col gap-2">
-        {loading && <div className="w-full flex items-center justify-center flex-none p-4">
+        {!hash && loading && <div className="w-full flex items-center justify-center flex-none p-4">
             <div className="w-10 aspect-square border-r rounded-full animate-spin" />
+        </div>}
+
+        {data.length == 0 || Object.values(contacts).length == 0 && <div className="w-full flex items-center justify-center flex-none p-4">
+            <span className="text-center text-white text-lg">No contacts</span>
         </div>}
         {Object.entries(contacts).map(([key, data]) => <div className="flex flex-col relative gap-4" key={key}>
             <span className="p-4 text-white font-bold text-2xl sticky top-0 bg-black uppercase z-20">{key}</span>
